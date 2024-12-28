8332affbdd9f67ebd06c097228a8e5bcbe35d872
Gypsy Magic <marek.daniluk@gmail.com>
2024-12-28 16:40:50 +0100

    image frame tests

--- test/image_frames_tests.cpp
+++ test/image_frames_tests.cpp
@@ -79,3 +79,67 @@ TEST_CASE("kra_imp_get_image_key_frames_count success", "[image_frames]")
     unsigned int frames_count = kra_imp_get_image_key_frames_count(KEY_FRAMES_XML.data(), KEY_FRAMES_XML.size());
     REQUIRE(frames_count == 4U);
 }
+
+TEST_CASE("kra_imp_read_image_key_frame null buffer", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(nullptr, 0ULL, 0U, &frame);
+    REQUIRE(result == KRA_IMP_PARAMS_ERROR);
+}
+
+TEST_CASE("kra_imp_read_image_key_frame xml_buffer_size=0", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(KEY_FRAMES_XML.data(), 0ULL, 0U, &frame);
+    REQUIRE(result == KRA_IMP_PARSE_ERROR);
+}
+
+TEST_CASE("kra_imp_read_image_key_frame empty buffer", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(EMPTY_XML.data(), EMPTY_XML.size(), 0U, &frame);
+    REQUIRE(result == KRA_IMP_PARSE_ERROR);
+}
+
+TEST_CASE("kra_imp_read_image_key_frame invalid xml", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(INVALID_KEY_FRAMES_XML.data(), INVALID_KEY_FRAMES_XML.size(), 0U, &frame);
+    REQUIRE(result == KRA_IMP_PARSE_ERROR);
+}
+
+TEST_CASE("kra_imp_read_image_key_frame invalid key_frame_index no frames", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(NO_KEY_FRAMES_XML.data(), NO_KEY_FRAMES_XML.size(), 0U, &frame);
+    REQUIRE(result == KRA_IMP_FAIL);
+}
+
+TEST_CASE("kra_imp_read_image_key_frame invalid key_frame_index", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(NO_KEY_FRAMES_XML.data(), NO_KEY_FRAMES_XML.size(), 4U, &frame);
+    REQUIRE(result == KRA_IMP_FAIL);
+}
+
+TEST_CASE("kra_imp_read_image_key_frame success", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(KEY_FRAMES_XML.data(), KEY_FRAMES_XML.size(), 0U, &frame);
+    REQUIRE(result == KRA_IMP_SUCCESS);
+    REQUIRE(frame._time == 0);
+    REQUIRE(std::strcmp(frame._frame, "layer3") == 0);
+    REQUIRE(frame._x == 0);
+    REQUIRE(frame._y == 0);
+}
+
+TEST_CASE("kra_imp_read_image_key_frame success with offset", "[image_frames]")
+{
+    kra_imp_image_key_frame_t frame;
+    kra_imp_error_code_e result = kra_imp_read_image_key_frame(KEY_FRAMES_XML.data(), KEY_FRAMES_XML.size(), 1U, &frame);
+    REQUIRE(result == KRA_IMP_SUCCESS);
+    REQUIRE(frame._time == 24U);
+    REQUIRE(std::strcmp(frame._frame, "layer3.f1") == 0);
+    REQUIRE(frame._x == 0);
+    REQUIRE(frame._y == 0);
+}
