49b094072d533734c9fd08432889b531caf342f3
Gypsy Magic <marek.daniluk@gmail.com>
2024-12-29 21:30:24 +0100

    fixed kra_imp_get_file_size and kra_imp_load_file signed/unsigned nonsense (thanks to compare code with doxygen)

--- inc/kra_imp/types.hpp
+++ inc/kra_imp/types.hpp
@@ -36,9 +36,8 @@ extern "C"
         KRA_IMP_FAIL = 0,           /**< Indicates the generic operation failed. */
         KRA_IMP_SUCCESS = 1,        /**< Indicates that the operation completed successfully. */
         KRA_IMP_PARAMS_ERROR = -1,  /**< Indicates operation failed due the invalid param(s). */
-        KRA_IMP_ARCHIVE_ERROR = -2, /**< Indicates archive (zip) internal operation failed . */
-        KRA_IMP_PARSE_ERROR = -3,   /**< Indicates that the parse operation failed. */
-        KRA_IMP_LZF_ERROR = -4,     /**< Indicates that the lzf decompress operation failed. */
+        KRA_IMP_PARSE_ERROR = -2,   /**< Indicates that the parse operation failed. */
+        KRA_IMP_LZF_ERROR = -3,     /**< Indicates that the lzf decompress operation failed. */
     } kra_imp_error_code_e;
     /**
      * @ingroup kra_imp
